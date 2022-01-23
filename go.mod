fa832e5f3d813df7027d134afbdb2aad1b5642f8
E-kenny <ekennyobiasogu@gmail.com>
2022-01-23 19:00:26 +0100

    proc

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/E-kenny/annuity
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0
--- http/handlers.go
+++ http/handlers.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func App() {
+	// load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println(fmt.Errorf("error occur %s", err))
+	}
 	http.HandleFunc("/benefit", benefit)
 	http.HandleFunc("/premium", premium)
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(  os.Getenv("PORT"), nil)
+	
 }
 
 func benefit(w http.ResponseWriter, r *http.Request) {
